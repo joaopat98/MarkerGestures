7b04887d27b852d90a76250c6940d6f732ac1f5a
joaopat98 <joaopat98@gmail.com>
2019-11-13 16:23:31 +0000

    Add base files

---
+++ Assets/SwitchMains.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class SwitchMains : MonoBehaviour
+{
+
+    public Transform[] mainTargets;
+    int current = 0;
+
+    void manageActive()
+    {
+        for (int i = 0; i < mainTargets.Length; i++)
+        {
+            if (i == current) mainTargets[i].gameObject.SetActive(true);
+            else mainTargets[i].gameObject.SetActive(false);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manageActive();
+    }
+
+    public void switchTarget()
+    {
+        current = (current + 1) % mainTargets.Length;
+        manageActive();
+        Vuforia.TrackerManager.Instance.GetStateManager().ReassociateTrackables();
+    }
+}
